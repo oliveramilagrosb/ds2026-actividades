5384f57d424b63b1eb9ff54f274a5097adcda0d5
oliveramilagrosb <oliveramilagrosb@gmail.com>
2026-04-16 15:54:50 -0300

    Ejercicio 1 - Variables y Operaciones

---
+++ ejercicios/c03-javascript/ej1-variables-y-operaciones/app.js
@@ -0,0 +1,12 @@
+const nombre = "Milagros Belén";
+const edad = 20;
+const materia = "Desarrollo de Software";
+
+console.log(`Me llamo ${nombre}, tengo ${edad} años y curso ${materia}`);
+
+let contador = 0;
+contador = contador +1;
+contador = contador +1;
+contador = contador +1;
+
+console.log("Contador final", contador);
